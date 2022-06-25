7729bc85e56705cecdf770818b21817bfc00cafe
Kumar <rajeev.f.kumar@capgemini.com>
2022-06-25 11:19:55 +0530

    Template Engine2

---
+++ Project1/TemplateEngine2.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/ReadableStream/Read.html');
+});
+
+app.get('/contact', function(req, res){
+    res.sendFile(__dirname + '/views/Contact.html');
+});
+
+//go to views folder views/profile.ejs
+app.get('/profile/:name', function(req, res){
+    var data = {age: 23, job:'Software Engineer', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile1', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
+console.log('Server Created Successfully');
